8d0fbbfa667669d2c3e1b5d63deeee1c9bd4e655
TheNelud <riwok@bk.ru>
2022-04-22 10:06:17 +0300

    create new config.xml

---
+++ COPCUA/src/main/java/ga/opc/ua/Distributor.java
@@ -0,0 +1,13 @@
+package ga.opc.ua;
+
+public class Distributor {
+    private String ipDB;
+    private String portDB;
+    private String userDB;
+    private String passDB;
+
+    private String selectNameTable;
+    private String selectColumnId;
+    private String selectColumnTag;
+    private String selectColumnInout;
+}
